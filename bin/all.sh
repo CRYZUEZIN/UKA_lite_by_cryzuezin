a423485e72f2b209aa294f05f29a8191750fe72c
Josué Martins <62780115+CRYZUEZIN@users.noreply.github.com>
2022-12-05 13:09:32 -0300

    Delete all.sh

--- bin/all.sh
+++
@@ -1,4 +0,0 @@
-sed -i "s+ajax=/sdcard+ajax=pwd+" unpack/*.sh
-sed -i "s+uka=/data/local/uka-lite+uka=pwd+" unpack/*.sh
-sed -i "s+py=/data/data/com.termux/files/usr/bin/python+#+" unpack/*.sh
-sed -i "s+py=/data/local/python39/usr/bin/python3+#+" unpack/*.sh
