266444db53e64ad853850fef51bf5b9d5e5d5e71
Josué Martins <62780115+CRYZUEZIN@users.noreply.github.com>
2022-12-06 08:12:38 -0300

    Update oneui_erofs.sh

--- bin/unpack/oneui_erofs.sh
+++ bin/unpack/oneui_erofs.sh
@@ -124,7 +124,7 @@ sed -i "s+ro.config.knox.ucm=1+#+" $tmp/system/system/build.prop
 sed -i "s+security.perf_harden=1+security.perf_harden=false+" $tmp/system/system/build.prop
 sed -i "s+ro.adb.secure=1+ro.adb.secure=0+" $tmp/system/system/build.prop
 cat $phh/fix.prop >> $tmp/system/system/build.prop
-cat $phh/phh_file_contexts >> $tmp/config/system/system_file_contexts
+cat $phh/phh_file_contexts >> $tmp/config/system_file_contexts
 echo " "
 
 full_avb=$($bin/avbtool info_image --image $tmp/product.img 2> $tmp/config/product_avb.log)
